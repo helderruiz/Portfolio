d04eac8be16d2567632751157ee095652ab127ca
HELDER RUIZ MARCELINO <110069838+helderruiz@users.noreply.github.com>
2022-11-04 19:09:44 -0300

    atualizei projetos

--- assets/js/contato.js
+++ assets/js/contato.js
@@ -3,9 +3,12 @@ botaoEnviar.addEventListener('click', clicar)
 
 const enviado = document.getElementById('enviado')
 const nome = document.getElementById('formGroupExampleInput')
+const email = document.getElementById('exampleFormControlInput1')
+const textArea = document.getElementById('exampleFormControlTextarea1')
+
 
 function clicar() {
-    if(nome.value.length == 0) {
+    if(nome.value.length == 0 || email.value.length == 0 || textArea.value.length == 0) {
         alert('Digite os campos acima!')
     } else {
         enviado.innerHTML = `<div id="enviado">Enviado com Sucesso</div>`
