0c41cc9db7dde1221712a4d8ff5e5883ce7fa597
HELDER RUIZ MARCELINO <110069838+helderruiz@users.noreply.github.com>
2024-06-02 21:21:16 -0300

    feito parte do menu mobile

---
+++ assets/js/menu-mobile.js
@@ -0,0 +1,10 @@
+const btnMobile = document.querySelector(".btn-mobile");
+const btnIcon = document.querySelector(".bi-list");
+const nav = document.querySelector(".nav");
+
+btnMobile.addEventListener("click", () => {
+    nav.classList.toggle('active')
+    
+    
+});
+
