eb849f6ffc5c6e72bdf96093e95bbea47132cc7a
HELDER RUIZ MARCELINO <110069838+helderruiz@users.noreply.github.com>
2024-06-03 11:46:32 -0300

    Feito o menu mobile, e agumas alteraçãoes no layout

--- assets/js/menu-mobile.js
+++ assets/js/menu-mobile.js
@@ -1,10 +1,13 @@
 const btnMobile = document.querySelector(".btn-mobile");
-const btnIcon = document.querySelector(".bi-list");
+const iamgemIcon = document.querySelector("#imagem");
 const nav = document.querySelector(".nav");
 
 btnMobile.addEventListener("click", () => {
-    nav.classList.toggle('active')
-    
-    
+  nav.classList.toggle("active");
+  const active = nav.classList.contains("active");
+  if (active) {
+    iamgemIcon.setAttribute('src', 'assets/img/sair-da-tela.png')
+  }else{
+    iamgemIcon.setAttribute('src', 'assets/img/barra-de-menu.png')
+  }
 });
-
